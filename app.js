5b26fef50b4884af6782e49518daa5d55113121e
vithursan sivakumaran <sivithu93240@gmail.com>
2019-03-13 11:09:23 +0100

    v3.1

--- app.js
+++ app.js
@@ -38,4 +38,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.listen(3000, () => {
+  console.log("Server is up and listnening on port " + 3000 + "...");
+})
+
 module.exports = app;
