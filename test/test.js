e6d6478af3be749d37d7f96c75e2cd3d3798aed0
vithursan sivakumaran <sivithu93240@gmail.com>
2019-03-14 15:58:33 +0100

    test intégration + test fonctionnel

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ import test from 'ava';
 import request from 'request';
 import axios from 'axios';
 
-
+//test fonctionnel
 test("La page d'index du site comprend bien un formulaire HTML", async t => {
   const response = await axios.get("https://mysterious-waters-98517.herokuapp.com");
     try {
@@ -13,11 +13,22 @@ test("La page d'index du site comprend bien un formulaire HTML", async t => {
     }
 });
 
+//test fonctionnel
 test("la page “ville” contient bien le nom de la ville qui lui a été passé en paramètre POST", async t => {
     try {
       const response = await axios.post("https://mysterious-waters-98517.herokuapp.com/ville", { name : 'paris'});
       var test = response.data.toString().includes('Voici votre ville : paris');
-      console.log(response.data.toString());
+      t.is(test, true);
+    } catch (e) {
+      t.fail("Une erreur s'est produite");
+    }
+})
+
+//test d'intégration
+test("la ville est inexistante", async t => {
+    try {
+      const response = await axios.post("https://mysterious-waters-98517.herokuapp.com/ville", { name : 'fzrgth'});
+      var test = response.data.toString().includes('Voici votre ville : Non existant');
       t.is(test, true);
     } catch (e) {
       t.fail("Une erreur s'est produite");
